e61d7525bcba3788aad13e37c408db09afaf0fc3
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2024-09-30 10:00:50 +0200

    Add files via upload

---
+++ convertidor_de_euro_a_dolar.py
@@ -0,0 +1,7 @@
+#Esto es un programa para convertir de euro a dolar
+print("Dime una cantidad €")
+num1 = int(input())
+num2 = float(1.10)
+#Realizamos la operación
+total = num1*num2
+print("El resultado de este cambio", num1, "*", num2, "=", total ," $")
